82509284fc6232467249aada8dc4dab5ff5cbd7c
Daniela Duarte <danielatavaresd@gmail.com>
2023-12-27 21:11:57 +0000

    JCD1.3.2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace JCD1._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+
+                int resultado = numero * i;
+
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+        }
+    }
+}
